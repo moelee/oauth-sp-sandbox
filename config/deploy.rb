397da60956117dc423d3d0afebb746c7bf65555b
Moses Lee <moses@fooala.com>
2009-01-16 17:30:29 -0500

    Change port to 3000

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,7 @@ require 'mongrel_cluster/recipes'
 set :application, "oauth-sp-sandbox"
 set :repository,  "git://github.com/moelee/oauth-sp-sandbox.git"
 set :branch, "master"
+set :deploy_via, :remote_cache
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
@@ -20,7 +21,7 @@ ssh_options[:host_key] = 'ssh-dss'
 ssh_options[:keys] = '~/.ssh/id_rsa'
 ssh_options[:paranoid] = false
 
-set :mongrel_config, "#{current_path}/config/mongrel_cluster.yml" 
+set :mongrel_conf, "#{current_path}/config/mongrel_cluster.yml" 
 
 set :use_sudo, false
 
