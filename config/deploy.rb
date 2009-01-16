853e47b3379d8d79196ed778af95b7f09135bb8e
Moses Lee <moses@fooala.com>
2009-01-16 12:07:30 -0500

    Capified

---
+++ config/deploy.rb
@@ -0,0 +1,21 @@
+set :application, "oauth-sp-sandbox"
+set :repository,  "git://github.com/moelee/oauth-sp-sandbox.git"
+
+# If you aren't deploying to /u/apps/#{application} on the target
+# servers (which is the default), you can specify the actual location
+# via the :deploy_to variable:
+# set :deploy_to, "/var/www/#{application}"
+
+# If you aren't using Subversion to manage your source code, specify
+# your SCM below:
+# set :scm, :subversion
+set :scm, :git
+set :branch, "master"
+
+#default_run_options[:pty] = true
+#set :ssh_options, {:forward_agent => true}
+#set :user, "deployer"
+
+role :app, "174.129.246.97"
+role :web, "174.129.246.97"
+role :db,  "174.129.246.97", :primary => true
