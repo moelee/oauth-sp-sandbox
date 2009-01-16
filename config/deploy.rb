a72f222707ef17d8e597193257239cca3e2c8f6f
Moses Lee <moses@fooala.com>
2009-01-16 15:26:14 -0500

    Adding necessary gem dependencies

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,6 @@
 set :application, "oauth-sp-sandbox"
 set :repository,  "git://github.com/moelee/oauth-sp-sandbox.git"
+set :branch, "master"
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
@@ -10,11 +11,17 @@ set :deploy_to, "/root/#{application}"
 # your SCM below:
 # set :scm, :subversion
 set :scm, :git
-set :branch, "master"
+set :user, 'root'
+ssh_options[:port] = 22
+ssh_options[:username] = 'root'
+ssh_options[:host_key] = 'ssh-dss'
+ssh_options[:keys] = '~/.ssh/id_rsa'
+ssh_options[:paranoid] = false
 
-default_run_options[:pty] = true
-set :ssh_options, {:forward_agent => true}
-set :user, "moseslee"
+set :use_sudo, false
+
+#default_run_options[:pty] = true
+#set :ssh_options, {:forward_agent => true}
 
 role :app, "174.129.246.97"
 role :web, "174.129.246.97"
