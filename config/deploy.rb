1a26bbc437907f1ff25ccdcac80cad953c3fdcd6
Moses Lee <moses@fooala.com>
2009-01-16 13:01:52 -0500

    Minor changes to capistrano deploy configs

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ set :repository,  "git://github.com/moelee/oauth-sp-sandbox.git"
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
+set :deploy_to, "/root/#{application}"
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
@@ -12,9 +12,9 @@ set :repository,  "git://github.com/moelee/oauth-sp-sandbox.git"
 set :scm, :git
 set :branch, "master"
 
-#default_run_options[:pty] = true
-#set :ssh_options, {:forward_agent => true}
-#set :user, "deployer"
+default_run_options[:pty] = true
+set :ssh_options, {:forward_agent => true}
+set :user, "moseslee"
 
 role :app, "174.129.246.97"
 role :web, "174.129.246.97"
