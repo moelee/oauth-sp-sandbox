57374685a150c593cb2c901e14c219ee47e3a581
Moses Lee <moses@fooala.com>
2009-01-23 13:59:15 -0500

    Turn off rails authenticity verification for now

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -4,10 +4,12 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   include AuthenticatedSystem
+  # Ignore authenticity token checks for now
+  skip_before_filter :verify_authenticity_token
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery # :secret => 'fcd7d9ba2d7b5219e1afaac318f51c12'
+  #protect_from_forgery # :secret => 'fcd7d9ba2d7b5219e1afaac318f51c12'
   
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,5 @@
 class PhotosController < ApplicationController
-  #before_filter :login_or_oauth_required
+  before_filter :login_or_oauth_required
   before_filter :find_user
   
 protected
