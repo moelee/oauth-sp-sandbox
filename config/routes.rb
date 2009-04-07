d552f5bda6296fcbdef0d608366d2cc50ece3d64
Moses Lee <moses@fooala.com>
2009-04-06 23:51:37 -0400

    Route root to login page

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ ActionController::Routing::Routes.draw do |map|
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   # map.root :controller => "welcome"
-  map.root :controller => 'users', :action => 'index'
+  map.root :controller => 'sessions', :action => 'new'
 
   # See how all your routes lay out with "rake routes"
 
