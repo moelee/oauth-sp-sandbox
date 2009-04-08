41430a4841a14cf8901c02d824041c41d8bbc981
Moses Lee <moses@fooala.com>
2009-04-07 22:20:04 -0400

    Set expires_on field after AccessToken creation

--- app/models/access_token.rb
+++ app/models/access_token.rb
@@ -17,4 +17,8 @@ class AccessToken<OauthToken
   def set_authorized_at
     self.authorized_at=Time.now
   end
+  
+  def set_expires_on
+    self.expires_on=Time.now + 60*60*24*7 # TODO: Need to set this expiration offset in a configuration file, currently set to 1 week
+  end
 end
