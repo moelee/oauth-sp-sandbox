6de64af8f1fa00bfb9246a5d6d1d4ceb7382f500
Moses Lee <moses@fooala.com>
2009-05-01 02:24:38 -0400

    Adjust header title

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,5 @@
 # This controller handles the login/logout function of the site.  
 class SessionsController < ApplicationController
-  # Be sure to include AuthenticationSystem in Application Controller instead
-  include AuthenticatedSystem
-
   # render new.rhtml
   def new
   end
@@ -75,28 +72,6 @@ private
     flash[:error] = message
     redirect_to(new_session_url)
   end
-=begin    
-  def create
-    logout_keeping_session!
-    user = User.authenticate(params[:login], params[:password])
-    if user
-      # Protects against session fixation attacks, causes request forgery
-      # protection if user resubmits an earlier form using back
-      # button. Uncomment if you understand the tradeoffs.
-      # reset_session
-      self.current_user = user
-      new_cookie_flag = (params[:remember_me] == "1")
-      handle_remember_cookie! new_cookie_flag
-      redirect_back_or_default('/')
-      flash[:notice] = "Logged in successfully"
-    else
-      note_failed_signin
-      @login       = params[:login]
-      @remember_me = params[:remember_me]
-      render :action => 'new'
-    end
-  end
-=end
 
 protected
   # Track failed login attempts
