fd39d19f11b51ce251c5f13215e3f0cbe9cd2562
Moses Lee <moses@fooala.com>
2009-05-01 04:04:19 -0400

    Fixing buth with find_token method

--- app/models/client_application.rb
+++ app/models/client_application.rb
@@ -12,10 +12,10 @@ class ClientApplication < ActiveRecord::Base
   
   attr_accessor :resource_ids
   
-  def self.find_token(token_key, resource)
+  def self.find_token(token_key)
     token=OauthToken.find_by_token(token_key.token, :include => :client_application)
     logger.info "Loaded #{token.token} which was authorized by (user_id=#{token.user_id}) on the #{token.authorized_at}"
-    return token if token.authorized?(resource)
+    return token if token.authorized?
     nil
   end
   
--- vendor/plugins/oauth_plugin/lib/oauth/rails/controller_methods.rb
+++ vendor/plugins/oauth_plugin/lib/oauth/rails/controller_methods.rb
@@ -96,13 +96,14 @@ module OAuth
       def verify_oauth_signature
         begin
           valid = ClientApplication.verify_request(request) do |token|
-            self.current_token = ClientApplication.find_token(token, params[:controller])
+            self.current_token = ClientApplication.find_token(token)
             logger.info "self=#{self.class.to_s}"
             logger.info "token=#{self.current_token}"
             # return the token secret and the consumer secret
             [(current_token.nil? ? nil : current_token.secret), (current_client_application.nil? ? nil : current_client_application.secret)]
           end
-          valid
+          within_scope = %w[oauth oauth_tokens].include?(params[:controller]) ? self.current_token.within_resource_scope?(params[:controller]) : nil
+          valid && within_scope
 #        rescue
 #          valid=false
         end
